19d2b695e8d087493171251f741bea9f5e77b4c8
Jens <jenslorden@gmail.com>
2023-05-31 00:30:20 +0200

    Initiate the tui by just drawing a box

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,24 @@
+use crossterm::{
+    // event::{self, Event as CEvent, KeyCode, KeyEvent},
+    terminal::{disable_raw_mode, enable_raw_mode},
+};
+use std::io;
 
+mod app;
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> Result<(), io::Error> {
+    enable_raw_mode().expect("Enable raw mode expect");
+
+    app::start_app().expect("start app ");
+
+    // restore terminal
+    disable_raw_mode()?;
+    // execute!(
+    //     terminal.backend_mut(),
+    //     LeaveAlternateScreen,
+    //     DisableMouseCapture
+    // )?;
+    // terminal.show_cursor()?;
+
+    Ok(())
 }
