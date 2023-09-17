3d816cf804aaa3c8b4bb8a4ae580cef410e8e91c
Jens <jenslorden@gmail.com>
2023-09-17 23:18:57 +0200

    working on scoreboard

--- src/scoreboard.rs
+++ src/scoreboard.rs
@@ -1,7 +1,16 @@
 use home;
+use serde::{Deserialize, Serialize};
+use serde_json::Result;
+use std::fs::File;
+
+#[derive(Serialize, Deserialize)]
+struct Scores {
+    scores: Vec<i16>,
+    names: Vec<String>,
+}
 
 pub struct Scoreboard {
-    scoreboard_path: String
+    scoreboard_path: String,
 }
 
 impl Scoreboard {
@@ -11,31 +20,40 @@ impl Scoreboard {
         }
     }
 
-    pub fn save_score(&self) {
-        let home_path: String = self.get_home_path();
-        if home_path == "" {
-            return
+    pub fn save_score(&self) -> Result<()> {
+        let file_path: String = self.get_scoreboard_path();
+        if file_path == "" {
+            return Ok(());
         }
 
-
+        Ok(())
     }
 
     pub fn get_score(&self) {
-        let home_path: String = self.get_home_path();
-        if home_path == "" {
-            return
+        let file_path: String = self.get_scoreboard_path();
+        if file_path == "" {
+            return;
+        }
+    }
+
+    fn load_scores(&self) -> Result<()> {
+        let file_path: String = self.get_scoreboard_path();
+        if file_path == "" {
+            return Ok(());
         }
 
+        let _file = File::open(file_path);
 
+        Ok(())
     }
 
     fn get_scoreboard_path(&self) -> String {
         let home_path: String = self.get_home_path();
         if home_path == "" {
-            return "".to_string()
+            return "".to_string();
         }
 
-        "".to_string()
+        format!("{}{}", home_path, self.scoreboard_path).to_string()
     }
 
     fn get_home_path(&self) -> String {
